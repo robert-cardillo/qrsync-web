3e6a460dedb76c2d5d69f820aed80075461dcd6b
Snow Blind <snowy.tmp@gmail.com>
2016-02-29 18:25:31 +0200

    added cookie utility functions

--- public/js/util.js
+++ public/js/util.js
@@ -51,3 +51,21 @@ function extractLinks(string) {
 	var result = string.replace(regex, '<a href="$1" target="_blank">$1</a>')
 	return result;	
 }
+
+function setCookie(cname, cvalue, exdays) {
+    var d = new Date();
+    d.setTime(d.getTime() + (exdays*24*60*60*1000));
+    var expires = "expires="+d.toUTCString();
+    document.cookie = cname + "=" + cvalue + "; " + expires;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i=0; i<ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0)==' ') c = c.substring(1);
+        if (c.indexOf(name) == 0) return c.substring(name.length, c.length);
+    }
+    return "";
+}
